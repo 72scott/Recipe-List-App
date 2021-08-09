7dcb8ee904c51bb19131a2e06f30f897db3be276
Scott Somerville <scottsomervilles@gmail.com>
2021-08-09 10:18:32 +1000

    build UI in the RecipeFeaturedView

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -14,47 +14,55 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     var body: some View {
         
-        VStack {
+        VStack (alignment: .leading, spacing: 0) {
             
             Text("Featured Recipes")
-        
-    GeometryReader { geo in
-        
-        TabView {
-            ForEach (0..<model.recipes.count) { index in
-                if model.recipes[index].featured == true {
-                    
-                    ZStack {
-                    Rectangle()
-                        .foregroundColor(.white)
-                        
-                        
-                        VStack(spacing: 0) {
-                            Image("lasagna")
-                                .resizable()
-                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                .clipped()
-                            Text("Lasagna")
-                                .padding(5)
+                .bold()
+                .padding(.leading)
+                .padding(.top, 20)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                
+                TabView {
+                    ForEach (0..<model.recipes.count) { index in
+                        if model.recipes[index].featured == true {
                             
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                    
+                                }
+                            }
+                            .frame(width: geo.size.width - 40,  height:  geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(15)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
                         }
-                    
+                    }
                 }
-                    .frame(width: geo.size.width - 40,  height:  geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(15)
-                    .shadow(radius: 10)
-            }
-            }
-        }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-    }
-        
-            VStack {
-            
-            
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
             }
             
+            VStack (alignment: .leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+                
+            }
+            .padding([.leading, .bottom])
         }
     }
 }
