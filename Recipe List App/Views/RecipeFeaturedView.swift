48d5834e095afe3bc558369b97514f1abb097dd3
Scott Somerville <scottsomervilles@gmail.com>
2021-08-09 11:45:36 +1000

    adjust padding in recipetabview text

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -17,7 +17,7 @@ struct RecipeFeaturedView: View {
             Text("Featured Recipes")
                 .bold()
                 .padding(.leading)
-                .padding(.top, 20)
+                .padding(.top, 40)
                 .font(.largeTitle)
             
             GeometryReader { geo in
